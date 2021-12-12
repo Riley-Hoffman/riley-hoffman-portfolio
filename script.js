71c6d347a44fb3fa8f8ef275088ed3ebd658bab9
Riley-Hoffman <hoffman.riley@gmail.com>
2021-12-12 14:08:55 -0500

    updated pseudocode.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// Controlled smooth scroll on first to sections, no smooth scroll on last sections
+// Controlled smooth scroll on first to sections, no smooth scroll on Skills/Contact to avoid jarring scroll visual.
 
 // prefers reduced media query.
 const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
 
-// Check if the media query matches or is not available.
+// Check user has selected Reduce Motion in their own settings.
 if (!reduceMotion || !reduceMotion.matches) {
   $(".aboutLink, .skip-link, .chevronLink").click(function () {
+    // slower scroll to About section
     $([document.documentElement, document.body]).animate(
       {
         scrollTop: $("#about").offset().top,
       }, 1500);
   });
+  // fast scroll to Projects section
   $(".projectsLink").click(function () {
     $([document.documentElement, document.body]).animate(
       {
