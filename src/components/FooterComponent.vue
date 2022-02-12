3304a0bb7e1e6fda57ea8f743e75c5708be08fae
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-12 15:19:29 -0500

    refactored footercomponent to utilize contactcomponent instead of repeating code.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,18 +1,7 @@
 <template>
   <!-- Footer Starts -->
     <footer class="wrapper" id="contact">
-        <div class="flexBox contactWrapper contactComponent">
-            <h2>Contact</h2>
-            <a href="mailto:riley.n.hoffman@gmail.com">riley.n.hoffman@gmail.com</a>
-            <div class="flexBox contactIcons">
-                <a href="https://github.com/Riley-Hoffman">
-                    <i class="fab fa-github" aria-hidden="true"></i><span class="sr-only">Github</span>
-                </a>
-                <a href="https://www.linkedin.com/in/riley-hoffman-014623213">
-                    <i class="fab fa-linkedin" aria-hidden="true"></i><span class="sr-only">Linkedin</span>
-                </a>
-            </div>
-        </div>
+        <ContactComponent />
         <!-- contactWrapper Wrapper Ends -->
         <div class="wrapper flexBox bottom">
             <p>Riley Hoffman <span aria-hidden="true">| </span>Web Developer</p>
@@ -21,3 +10,11 @@
     </footer>
     <!-- Footers Ends -->
 </template>
+<script>
+import ContactComponent from '@/components/ContactComponent.vue'
+export default {
+  components: {
+    ContactComponent
+  }
+}
+</script>
