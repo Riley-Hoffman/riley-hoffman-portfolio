453edf3183542018f16f5ff6eaaef88a535408aa
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-12 10:33:52 -0500

    pushing refactor back to branch

---
+++ src/views/Projects.vue
@@ -0,0 +1,136 @@
+<template>
+   <!-- Projects Section Starts -->
+    <main id="main">
+        <section class="projects" id="projects">
+            <div class="wrapper flexBox">
+                <h2>Projects</h2>
+                <ul>
+                    <li class="projectBox">
+                        <div class="projectInfo">
+                            <h3>Out Of Context</h3>
+                            <h4>React, JSX, AXIOS, SCSS</h4>
+                            <p>
+                                Created with React, this app allows users to search by movie
+                                name and displays GIFs based on themes (keywords) from the
+                                movie (love, war, sports etc). Three GIFs are displayed
+                                initially, with their keywords underneath. By clicking, the
+                                user can cycle through all GIFs per keyword, or look at other
+                                keywords associated with the movie, returning new GIFs!
+                            </p>
+                            <h5>Out Of Context Links:</h5>
+                            <a href="https://focused-varahamihira-abf5da.netlify.app/" target="_blank">Live</a>
+                            <a href="https://github.com/dearJuno/outofContext" target="_blank">Repo</a>
+                        </div>
+                        <div class="imgBox">
+                            <a href="https://focused-varahamihira-abf5da.netlify.app/" title="Visit Out Of Context live site."
+                                target="_blank">
+                                <img srcset="
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782917/out-of-context3x_krpnlk.webp 1280w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782917/out-of-context2x_mtbvqx.webp 900w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782917/out-of-context1x_ojtnhg.webp 550w
+                                " sizes="(max-width: 900px) 100vw, 900px" width="887" height="887"
+                                    src="https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782917/out-of-context_oeh1on.png"
+                                    alt="Screenshot of the Out Of Context app." title="Out Of Context">
+                                <div class="oval"></div>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="projectBox">
+                        <div class="imgBox">
+                            <a href="https://relaxed-visvesvaraya-8807c8.netlify.app/" title="Visit Infinity Corkboard live site."
+                                target="_blank">
+                                <img srcset="
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783133/infinity-corkboard3x_vnzfw3.webp 1280w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783134/infinity-corkboard2x_qc17n0.webp 900w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783133/infinity-corkboard1x_wsb1v7.webp 550w
+                                " sizes="(max-width: 900px) 100vw, 900px" width="887" height="887"
+                                    src="https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783133/infinity-corkboard_aanwxx.png"
+                                    alt="Screenshot of the Infinity Corkboard app." title="Infinity Corkboard">
+                                <div class="oval"></div>
+                            </a>
+                        </div>
+                        <div class="projectInfo">
+                            <h3>Infinity Corkboard</h3>
+                            <h4>React, JSX, REST API, CSS</h4>
+                            <p>
+                                This React based app presents the user with a 4X4 grid of
+                                photos from NASA APIs' Astronomy Picture of the Day endpoint.
+                                The user can click any images they wish to change, which swaps
+                                it out for a new one in the same position. When the user is
+                                pleased with how the grid looks, they may save it by printing
+                                to pdf. Print output is styled so the grid appears with no
+                                other page elements in the saved file.
+                            </p>
+                            <h5>Infinity Corkboard Links:</h5>
+                            <a href="https://relaxed-visvesvaraya-8807c8.netlify.app/" target="_blank">Live</a>
+                            <a href="https://github.com/Riley-Hoffman/riley-hoffman-project-three"
+                                target="_blank">Repo</a>
+                        </div>
+                    </li>
+
+                    <li class="projectBox">
+                        <div class="projectInfo">
+                            <h3>Weather Scout</h3>
+                            <h4>HTML, Javascript, REST API, SCSS</h4>
+                            <p>
+                                With this app, users can search by city and get corresponding
+                                weather data. 'Right Now' or 'Future Forecast' can be
+                                specified. 'Right Now' returns a weather description with
+                                associated icon, and the current temperature. Future forecast
+                                returns a grid of dates with forecast temperatures. Created
+                                with Javascript, HTML and CSS.
+                            </p>
+                            <h5>Weather Scout Links:</h5>
+                            <a href="https://romantic-kowalevski-f7a14c.netlify.app/" title="Weather Scout"
+                                target="_blank">Live</a>
+                            <a href="https://github.com/Linda-Columbus-Riley-Hoffman-Developers/weatherScout"
+                                target="_blank">Repo</a>
+                        </div>
+                        <div class="imgBox">
+                            <a href="https://romantic-kowalevski-f7a14c.netlify.app/" target="_blank" title="Visit Weather Scout live site.">
+                                <img srcset="
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782603/weather-scout3x_hihe8q.webp 1280w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782602/weather-scout2x_h1wuto.webp 900w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782603/weather-scout1x_a5fbv9.webp 550w
+                                " sizes="(max-width: 900px) 100vw, 900px" width="887" height="887"
+                                    src="https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642782602/weather-scout_hglg66.png"
+                                    alt="Screenshot of the Weather Scout app." title="Weather Scout">
+                                <div class="oval"></div>
+                            </a>
+                        </div>
+                    </li>
+
+                    <li class="projectBox">
+                        <div class="imgBox">
+                            <a href="https://vigilant-albattani-29bee8.netlify.app/" title="Visit Delicious live site." target="_blank">
+                                <img srcset="
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783284/delicious3x_pgwxjq.webp 1280w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783284/delicious2x_bkve7w.webp 900w,
+                                https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642783281/delicious1x_g6etos.webp 550w
+                                " sizes="(max-width: 900px) 100vw, 900px" width="887" height="887"
+                                    src="https://res.cloudinary.com/riley-hoffman-web-developer/image/upload/q_auto:low/v1642784060/delicious_aozvru.png"
+                                    alt="Screenshot of Delicious, a PSD conversion project." title="Delicious">
+                                <div class="oval"></div>
+                            </a>
+                        </div>
+                        <div class="projectInfo">
+                            <h3>Delicious</h3>
+                            <h4>HTML, SCSS</h4>
+                            <p>
+                                A multi-page PSD conversion project for a fictional restaurant
+                                chain.
+                            </p>
+                            <h5>Delicious Links:</h5>
+                            <a href="https://vigilant-albattani-29bee8.netlify.app/" target="_blank">Live</a>
+                            <a href="https://github.com/Riley-Hoffman/riley_hoffman_project_1_delicious"
+                                target="_blank">Repo</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- Wrapper Ends -->
+        </section>
+        <!-- Project Section Ends -->
+    </main>
+</template>
