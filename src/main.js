59d4f0d2c1a7b078ea0d31eb0944a3fbff718578
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-14 11:35:44 -0500

    setting up vue PWA

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/style.css'
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(router)
