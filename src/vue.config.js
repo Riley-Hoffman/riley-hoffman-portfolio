44c1bb865d008aa923a26063a3bb0c279b420b60
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-14 14:05:06 -0500

    configured serviceWorker

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,7 +1,18 @@
+const { GenerateSW } = require('workbox-webpack-plugin')
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'development' ? '/vuejs-pwa/' : '',
+  configureWebpack: {
+    plugins: [new GenerateSW()]
+  },
   pwa: {
     themeColor: '#00050f',
-    workboxPluginMode: 'InjectManifest',
+    workboxPluginMode: 'GenerateSW',
+    msTileColor: '#b91d47',
+    appleMobileWebAppCache: 'yes',
+    manifestOptions: {
+      background_color: '#42b983'
+    },
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
       swSrc: 'src/sw.js'
