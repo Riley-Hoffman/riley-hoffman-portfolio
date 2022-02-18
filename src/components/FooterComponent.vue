22cce5aa5bff7b08195ff8522a26dc61a7eb4638
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-18 10:11:53 -0500

    added link to resume in footer with styling and hover/active effects. removed animation from .smoke element in light mode.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Footer Starts -->
-    <footer class="wrapper" :class="splashFooter" id="contact">
+    <footer class="wrapper flexBox" :class="splashFooter" id="contact">
         <div class="wrapper flexBox contactWrapper contactComponent">
             <h2>Contact</h2>
             <a href="mailto:riley.n.hoffman@gmail.com">riley.n.hoffman@gmail.com</a>
@@ -9,6 +9,7 @@
         <!-- contactWrapper Wrapper Ends -->
         <div class="wrapper flexBox bottom">
             <p>Riley Hoffman <span aria-hidden="true">| </span>Web Developer</p>
+            <a href="./riley-hoffman-resume.pdf" class="resume">Resume<i class="fa-solid fa-file-arrow-down"></i></a>
         </div>
         <!-- Bottom Wrapper Ends -->
     </footer>
