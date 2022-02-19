05c39ad53c7e5d8c6d348d05acff7fe7404ce9e0
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-19 17:05:16 -0500

    added popup modal to display contact form on homepage when contact me button is clicked.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,14 +14,17 @@
             <span>Web </span><span class="outlined">Developer</span>
           </p>
           <!-- tealContactButton class is to facilitate media query targetting -->
-          <a
-            href="mailto:riley.n.hoffman@gmail.com"
+          <button
             class="tealContactButton headerLink"
-            ><span class="target">Contact<span class="me"> Me</span> </span
-            ><i class="fa-solid fa-arrow-right-long" aria-hidden="true"></i
-          ></a>
-          <div class="modal">
-          <FormComponent />
+            @click="toggleModal"
+            ><span class="target">Contact<span class="me"> Me</span></span
+            ></button>
+          <div class="modal" v-if="!showModal">
+              <button class="modalClose" @click="toggleModal">
+              <i class="fa-solid fa-xmark" aria-hidden="true"></i>
+              <span class="sr-only">Close contact form</span>
+              </button>
+              <FormComponent />
           </div>
           <div class="contactWrapper">
             <SocialComponent />
@@ -48,9 +51,19 @@
 import SocialComponent from '@/components/SocialComponent.vue'
 import FormComponent from '@/components/FormComponent.vue'
 export default {
+  data () {
+    return {
+      showModal: 'false'
+    }
+  },
   components: {
     SocialComponent,
     FormComponent
+  },
+  methods: {
+    toggleModal () {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
