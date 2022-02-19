4f5d7e63526330f54ab41b85caba13047816f0c7
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-19 14:10:17 -0500

    moved form html into it's own component started coding modal with form for home page.

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,27 @@
+<template>
+<form
+        action="https://formspree.io/f/xwkyonza"
+        method="POST"
+      >
+      <div class="flexBox inputBox">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Your name.." />
+
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email.."
+        />
+      </div>
+        <label for="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Write something.."
+        ></textarea>
+
+        <button value="Submit" class="contactFormButton">Submit</button>
+      </form>
+</template>
