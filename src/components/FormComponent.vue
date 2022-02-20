69d56c05a3b03ae22230d475585c52596c81beac
Riley-Hoffman <hoffman.riley@gmail.com>
2022-02-20 10:43:21 -0500

    added fieldset element to form and restyled accordingly. added sr-only field set legend. More styling to contain modal elements in media queries.

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -3,24 +3,29 @@
         action="https://formspree.io/f/xwkyonza"
         method="POST"
       >
-      <div class="flexBox inputBox">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" placeholder="Your name.." />
+      <fieldset>
+        <legend class="sr-only">Send me an email.</legend>
+        <div class="flexBox fieldsetBox">
+          <div class="flexBox inputBox">
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Your name.." />
 
-        <label for="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Your email.."
-        />
-      </div>
-        <label for="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          placeholder="Write something.."
-        ></textarea>
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email.."
+            />
+          </div>
+            <label for="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Write something.."
+            ></textarea>
+        </div>
+      </fieldset>
 
         <button value="Submit" class="contactFormButton">Submit</button>
       </form>
