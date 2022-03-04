be8f807739bc7cadf4534f84fdffb3d22e560b1e
Riley Hoffman <hoffman.riley@gmail.com>
2022-03-04 07:47:16 -0500

    added aria-label to resume download

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -13,7 +13,7 @@
     <!-- contactWrapper Wrapper Ends -->
     <div class="wrapper flexBox bottom">
       <p>Riley Hoffman <span class="verticalLine" aria-hidden="true">| </span><span class="webDeveloper">Web Developer</span></p>
-      <a href="./riley-hoffman-resume.pdf" class="resume" download>
+      <a href="./riley-hoffman-resume.pdf" class="resume" aria-label="Download my resumay" download>
       Resume
       <font-awesome-icon icon="file-arrow-down" class="fa-solid fa-file-arrow-down" aria-hidden="true" />
       </a>
