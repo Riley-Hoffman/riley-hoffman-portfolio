14723b7efbf165a99f143cbd81043acca6a06ead
Riley Hoffman <hoffman.riley@gmail.com>
2022-03-06 15:42:52 -0500

    refactored skills page to dynamically render skills list from an array. Projects page next.

---
+++ src/components/SkillsListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    <div class="skillsIconBox" v-bind:class="{ noAnimation: this.safari }">
+      <i :class="icon" aria-hidden="true" v-if="devicon"></i>
+      <font-awesome-icon :icon="icon" class="fa-solid" aria-hidden="true" v-else-if="!devicon" />
+    </div>
+    <p>{{ skill }}</p>
+  </li>
+</template>
+<script>
+export default {
+  props: ['icon', 'skill', 'devicon'],
+  data () {
+    return {
+      safari: navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('OPR') && !navigator.userAgent.includes('Chrome') && !navigator.userAgent.includes('Android')
+    }
+  }
+}
+</script>
